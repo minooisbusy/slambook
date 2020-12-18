33bd8a0d1f106ed6cada6003a9076700b12681fe
minwoo <minwoo03d0@gmail.com>
2020-12-18 18:59:54 +0900

    Writing.. MapPoint class ver0.1

--- project/0.4/include/myslam/mappoint.h
+++ project/0.4/include/myslam/mappoint.h
@@ -24,7 +24,7 @@
 
 namespace myslam
 {
-    
+
 class Frame;
 class MapPoint
 {
@@ -32,35 +32,67 @@ public:
     typedef shared_ptr<MapPoint> Ptr;
     unsigned long      id_;        // ID
     static unsigned long factory_id_;    // factory id
-    bool        good_;      // wheter a good point 
+    bool        good_;      // wheter a good point
     Vector3d    pos_;       // Position in world
-    Vector3d    norm_;      // Normal of viewing direction 
-    Mat         descriptor_; // Descriptor for matching 
-    
-    list<Frame*>    observed_frames_;   // key-frames that can observe this point 
-    
+    Vector3d    norm_;      // Normal of viewing direction
+    Mat         descriptor_; // Descriptor for matching
+
+    list<Frame*>    observed_frames_;   // key-frames that can observe this point
+
     int         matched_times_;     // being an inliner in pose estimation
-    int         visible_times_;     // being visible in current frame 
-    
+    int         visible_times_;     // being visible in current frame
+
     MapPoint();
-    MapPoint( 
-        unsigned long id, 
-        const Vector3d& position, 
-        const Vector3d& norm, 
-        Frame* frame=nullptr, 
-        const Mat& descriptor=Mat() 
+    MapPoint(
+        unsigned long id,
+        const Vector3d& position,
+        const Vector3d& norm,
+        Frame* frame=nullptr,
+        const Mat& descriptor=Mat()
     );
-    
+
     inline cv::Point3f getPositionCV() const {
         return cv::Point3f( pos_(0,0), pos_(1,0), pos_(2,0) );
     }
-    
+
     static MapPoint::Ptr createMapPoint();
-    static MapPoint::Ptr createMapPoint( 
-        const Vector3d& pos_world, 
+    static MapPoint::Ptr createMapPoint(
+        const Vector3d& pos_world,
         const Vector3d& norm_,
         const Mat& descriptor,
         Frame* frame );
+};
+}
+
+namespace goslam
+{
+class KeyFrame;
+class Map;
+class Frame;
+class MapPoint
+{
+
+public:
+    MapPoint(const cv::Mat &Pos, KeyFrame* pRefKF, Map* pMap);
+    MapPoint(const cv::Mat &Pos, Map* pMap, Frame* pFrame, const int &idxF);// Frame이있는가??
+
+    void SetWorldPos(const cv::Mat &Pos);
+    cv::Mat GetWorldPos();
+
+    cv::Mat GetNormal();
+    KeyFrame* GetReferenceKeyFrame();
+
+    std::map<KeyFrame*, size_t> GetObservations();
+    int Observations();
+
+    void AddObservation(KeyFrame* pKF, size_t idx);
+    void EraseObservation(KeyFrame* pKF);
+
+    int GetIndexInKeyFrame(KeyFrame* pKF);
+protected:
+ Mat mWorldPos; // Its 3D position X_{w,i} in the WCS.
+
+
 };
 }
 
